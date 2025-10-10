450a1296909d19332cc719c6c3fc7510ac3cfbcf
Subiksha-T-V <velumanithiyagu@gmail.com>
2025-10-10 15:26:26 +0530

    rpsc

---
+++ basic.js
@@ -0,0 +1,19 @@
+// let a={roll:1,name:"aaa"};
+// let b=a;
+// a.roll=2;
+// console.log(a,b);
+
+// let a=5;
+// console.log(9<<2);
+
+// function sum(a)
+// {
+//     return a+1;
+// }
+// console.log(sum(9));
+
+// const sum=(a)=>{
+//     return a++;
+// }
+// console.log(sum(1));
+
